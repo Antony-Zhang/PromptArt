fbc9a0e225b236aceb6b49322a163687b2c246f8
小白 <785541937@qq.com>
2023-06-28 14:44:01 +0800

    诗歌转白话文，诗歌意象提取（暂时废弃）

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import WS
-import prompt.mood_analysis
-import prompt.image_analysis as image_analysis
-import prompt.poem_trans as poem_trans
 import prompt.desc_expand as desc_expand
+
 
 if __name__ == "__main__":
     print("请开始对话吧!!")
@@ -18,4 +15,13 @@ if __name__ == "__main__":
 
         # 描述扩写
         desc_expand.DescExpand.expand(txt)
+        # # 情绪识别
+        # MoodAnalysis.MoodAnalysis.mood_analyze
+        # 古诗翻译
+        #Poem_Trans.Poem_Trans.poemTrans(txt)
+        # 古诗白话文赏析
+        # Pu_Trans.Pu_Trans.puTrans(txt)
+
+        # 意象提取
+        #Image_Get.Image_Get.getImage(txt)
         txt = input("\n(输入-1结束对话)文案:")
