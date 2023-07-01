c2449ee87bcb73e37ad243ce96fbc7f046b596da
Fishead_East <91858912+Antony-Zhang@users.noreply.github.com>
2023-07-01 10:54:11 +0800

    自动检索（标题+作者+原文+翻译）

--- webSearch/web_search.py
+++ webSearch/web_search.py
@@ -9,22 +9,44 @@
 """
 from bs4 import BeautifulSoup as bs
 import requests
+import re   # 正则表达式
 
 
+url = "https://so.gushiwen.cn"
 # 循环调试
 for x in range(20):
     poem = input("需要查询的诗名或诗句：")
-    url = "https://so.gushiwen.cn/search.aspx?value=" + poem + "&valuej=" + poem[0]
-    req = requests.get(url=url)
-    req.encoding = "utf-8"
-    html = req.text  # 返回的html内容
-    # 从html文件中提取数据(作者与古诗)
-    soup = bs(html, features="html.parser")  # 返回解析对象
-    author = soup.find_all("p", class_="source")
-    poems = soup.find_all("div", class_="contson")
-    # 将作者与古诗对应，转化为dict
-    author_poems = dict(zip(author, poems))
-    for author, poem in author_poems.items():
-        print("作者:"+author.text.strip())
-        print("古诗:"+poem.text.strip())
+    # 进入搜索界面
+    url_search = url + "/search.aspx?value=" + poem + "&valuej=" + poem[0]
+    req_search = requests.get(url=url_search)
+    req_search.encoding = "utf-8"
+    html = req_search.text  # 返回的html内容
+    soup_search = bs(html, features="html.parser")  # 返回解析对象
+
+    # 定位第一首古诗，并跳转至详情页
+    new_url = soup_search.find("div", {"class": "sons"}).find('a').attrs['href']  # 获取古诗跳转链接
+    url_poetry = url + new_url
+    req_poetry = requests.get(url=url_poetry)
+    req_poetry.encoding = "utf-8"
+    soup_poetry = bs(req_poetry.text, features="html.parser")
+
+    # 提取数据
+    title = soup_poetry.find('h1')
+    if len(title) == 0:
+        print("未找到古诗")
+        continue
+    print("标题："+title.text.strip())
+    author = soup_poetry.find("p", class_="source")
+    print("作者:"+author.text.strip())
+    contents = soup_poetry.find("div", class_="contson")
+    print("古诗:"+contents.text.strip())
+    trans = soup_poetry.find("div", id=re.compile(r'fanyi+\d')).find('p')
+    print("翻译:"+trans.text.strip().lstrip("译文"))
+    # # 结果转化为dict
+    # author_poems = dict(zip(title, author, contents, trans))
+    # for title, author, contents, trans in author_poems.items():
+    #     print("标题："+title.text.strip())
+    #     print("作者:"+author.text.strip())
+    #     print("古诗:"+contents.text.strip())
+    #     print("翻译:"+trans.text.strip())
 
