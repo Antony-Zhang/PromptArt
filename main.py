b10eaa76e4bb4381fdb26111271b6bc87ff55f23
Fishead_East <91858912+Antony-Zhang@users.noreply.github.com>
2023-07-03 13:16:47 +0800

    Web检索重构

--- main.py
+++ main.py
@@ -5,26 +5,14 @@ from LLM.spark_desk import SparkDesk
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SimpleSequentialChain
 
+from webSearch.web_search_poem import search_poem
+
 if __name__ == "__main__":
+    # 创建大模型
     llm = SparkDesk()
 
-    # test
-    prompt_1 = PromptTemplate(
-        input_variables=["lastname"],
-        template="我的邻居姓{lastname}，他生了个儿子，给他儿子起个名字，只需要一个",
-    )
-    chain_1 = LLMChain(llm=llm,
-                       prompt=prompt_1)
-    # 创建第二条链
-    prompt_2 = PromptTemplate(
-        input_variables=["child_name"],
-        template="邻居的儿子名字叫{child_name}，给他起一个小名",
-    )
-    chain_2 = LLMChain(llm=llm, prompt=prompt_2)
+    poem_input = input("请输入诗句：")
+    # 检索古诗
+    poetry_dict = search_poem(poem_input)
 
-    # 链接两条链
-    overall_chain = SimpleSequentialChain(chains=[chain_1, chain_2], verbose=True)
 
-    # 执行链，只需要传入第一个参数
-    catchphrase = overall_chain.run("王")
-    print(catchphrase)
--- webSearch/web_search_poem.py
+++ webSearch/web_search_poem.py
@@ -2,49 +2,53 @@
 # @Time : 2023/6/30 15:27
 # @Author : Fishead_East
 # @Email : ytzd2696@foxmail.com
-# @File : web_search.py
+# @File : web_search_poem.py
 # @Project : PromptArt
-"""
-    web检索古诗的全文与释义
-"""
-from bs4 import BeautifulSoup as bs
-import requests
-import re   # 正则表达式
 
+from typing import Optional, Any
+from bs4 import BeautifulSoup as BS
+import requests
+import re  # 正则表达式
 
 url = "https://so.gushiwen.cn"
-# 循环调试
-for x in range(20):
-    poem = input("###需要查询的诗名或诗句：")
+
+
+def search_poem(poem) -> Optional[dict[Any, Any]]:
+    """
+    实时Web检索古诗(第一首)
+    :param poem: 诗句
+    :return: 古诗信息的字典["title", "author", "contents", "trans"]
+    """
     # 进入搜索界面
     url_search = url + "/search.aspx?value=" + poem + "&valuej=" + poem[0]
     req_search = requests.get(url=url_search)
     req_search.encoding = "utf-8"
     html = req_search.text  # 返回的html内容
-    soup_search = bs(html, features="html.parser")  # 返回解析对象
+    soup_search = BS(html, features="html.parser")  # 返回解析对象
 
     # 定位第一首古诗，并跳转至详情页
     new_url = soup_search.find("div", {"class": "sons"}).find('a').attrs['href']  # 获取古诗跳转链接
     url_poetry = url + new_url
     req_poetry = requests.get(url=url_poetry)
     req_poetry.encoding = "utf-8"
-    soup_poetry = bs(req_poetry.text, features="html.parser")
+    soup_poetry = BS(req_poetry.text, features="html.parser")
 
     # 提取数据
     title = soup_poetry.find('h1').text.strip()
     if len(title) == 0:
         print("未找到古诗")
-        continue
+        return None
+
     author = soup_poetry.find("p", class_="source").text.strip()
     contents = soup_poetry.find("div", class_="contson").text.strip()
     trans = soup_poetry.find("div", id=re.compile(r'fanyi+\d')).find('p').text.strip().lstrip("译文")
 
     # 结果转化为dict
-    keys = ["标题", "作者", "古诗", "翻译"]
+    keys = ["title", "author", "contents", "trans"]
     values = [title, author, contents, trans]
     poetry_dict = dict(zip(keys, values))
 
-    # 打印
-    for key, value in poetry_dict.items():
-        print(key+": "+value)
-
+    return poetry_dict
+    # # 打印
+    # for key, value in poetry_dict.items():
+    #     print(key+": "+value)
