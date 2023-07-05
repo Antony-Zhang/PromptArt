c2ef5e1b52297fbbbbf8efaf5ec1aa04133bf617
Fishead_East <91858912+Antony-Zhang@users.noreply.github.com>
2023-07-05 15:41:09 +0800

    Embed自定义而不重写

--- LLM/spark_desk_embedding.py
+++ LLM/spark_desk_embedding.py
@@ -17,26 +17,33 @@ from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from LLM.webInteract.web_param import WsParamEmb
 
 
-class SparkDeskEmbeddings(HuggingFaceEmbeddings):
-    """重写HuggingFaceEmbeddings加载类"""
-    def __init__(self, **kwargs: Any):
-        super().__init__(**kwargs)
+class SparkDeskEmbedding(object):
+    """
+    讯飞星火的Embedding模型
+    """
+    url = r'https://knowledge-retrieval.cn-huabei-1.xf-yun.com/v1/aiui/embedding/query'
+    APPID: str = os.getenv("APPID")
+    APIKey: str = os.getenv("APIKEY")
+    APISecret: str = os.getenv("APISECRET")
 
-    def embed_documents(self, texts: List[str]) -> List[List[float]]:
-        """Compute doc embeddings using a HuggingFace transformer model.
-
-        Args:
-            texts: The list of texts to embed.
-
-        Returns:
-            List of embeddings, one for each text.
+    def _get_param(self, text) -> Mapping[str, Any]:
         """
-        texts = list(map(lambda x: x.replace("\n", " "), texts))
-        embeddings = self.client.encode(texts, normalize_embeddings=True)
-        return embeddings.tolist()
+        组织请求消息
+        :param text: 待向量化的文本
+        :return:
+        """
+        param_dict = {
+            'header': {
+                'app_id': self.APPID
+            },
+            'payload': {
+                'text': text
+            }
+        }
+        return param_dict
 
-    def embed_query(self, text: str) -> List[float]:
-        """Compute query embeddings using a HuggingFace transformer model.
+    def embed_query(self, text: str,) -> List[float]:
+        """Compute query embeddings using the Spark Desk Model.
 
         Args:
             text: The text to embed.
@@ -44,65 +51,42 @@ class SparkDeskEmbeddings(HuggingFaceEmbeddings):
         Returns:
             Embeddings for the text.
         """
-        text = text.replace("\n", " ")
-        embedding = self.client.encode(text, normalize_embeddings=True)
-        return embedding.tolist()
-
-
-# class SparkDeskEmbedding(LLM):
-#     """
-#     讯飞星火的Embedding模型
-#     """
-#     url = r'https://knowledge-retrieval.cn-huabei-1.xf-yun.com/v1/aiui/embedding/query'
-#     APPID: str = os.getenv("APPID")
-#     APIKey: str = os.getenv("APIKEY")
-#     APISecret: str = os.getenv("APISECRET")
-#
-#     @property
-#     def _llm_type(self) -> str:
-#         return "SparkDeskEmbedding"
-#
-#     @property
-#     def _identifying_params(self) -> Mapping[str, Any]:
-#         _param_dict = {
-#             "url": self.url,
-#             "APPID": self.APPID,
-#             "APIKey": self.APIKey,
-#             "APISecret": self.APISecret
-#         }
-#         return _param_dict
-#
-#     def _get_param(self, text) -> Mapping[str, Any]:
-#         """
-#         组织请求消息
-#         :param text: 待向量化的文本
-#         :return:
-#         """
-#         param_dict = {
-#             'header': {
-#                 'app_id': self.APPID
-#             },
-#             'payload': {
-#                 'text': text
-#             }
-#         }
-#         return param_dict
-#
-#     def _call(
-#             self,
-#             prompt: str,
-#             stop: Optional[List[str]] = None,
-#             run_manager: Optional[CallbackManagerForLLMRun] = None,
-#     ) -> str:
-#         ws_param = WsParamEmb(self.url, self.APPID, self.APIKey, self.APISecret)
-#         wsUrl = ws_param.create_url()
-#         param_dict = self._get_param(prompt)
-#         response = requests.post(url=wsUrl, json=param_dict)
-#         result = json.loads(response.content.decode('utf-8'))
-#         print(result)
-#         return result
+        ws_param = WsParamEmb(self.url, self.APPID, self.APIKey, self.APISecret)
+        wsUrl = ws_param.create_url()
+        param_dict = self._get_param(text)
+        response = requests.post(url=wsUrl, json=param_dict)    # 得到响应串
+        result_dict = json.loads(response.content.decode('utf-8'))
+        embed = json.loads(result_dict['payload']['text']['vector'])
+        return embed
 
 
 if __name__ == '__main__':
-    # llm_emb = SparkDeskEmbedding()
-    # print(llm_emb("OK"))
+    llm_embed = SparkDeskEmbedding()
+    embed1 = llm_embed.embed_query("你好吗？")
+    print(embed1)
+    print(len(embed1))
+    print(embed1[:5])
+
+
+# class SparkDeskEmbeddings(HuggingFaceEmbeddings):
+#     """重写HuggingFaceEmbeddings加载类"""
+#
+#     client: Any  #: :meta private:
+#     model_name: str = "SparkDeskEmbeddings"
+#
+#     def __init__(self, **kwargs: Any):
+#         super().__init__(**kwargs)
+#         # self.client即向量化工具，为sentence_transformers包中的类
+#
+#     def embed_query(self, text: str) -> List[float]:
+#         """Compute query embeddings using a HuggingFace transformer model.
+#
+#         Args:
+#             text: The text to embed.
+#
+#         Returns:
+#             Embeddings for the text.
+#         """
+#         text = text.replace("\n", " ")
+#         embedding = self.client.encode(text, normalize_embeddings=True)
+#         return embedding.tolist()
